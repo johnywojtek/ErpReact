77a13bc4887d93ed3c25c4f932dc3e94261c52fe
johnywojtek <johnywojtek@gmail.com>
2019-09-20 17:59:40 +0200

    Updates

---
+++ precache-manifest.e7b98cdcb995b045489c0a61ed8edf95.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb3c739a84e6a2e587b6e0efcc0d400",
+    "url": "/ErpReact/index.html"
+  },
+  {
+    "revision": "577fc81b573439f2cddd",
+    "url": "/ErpReact/static/css/2.659ddf1d.chunk.css"
+  },
+  {
+    "revision": "8b83e0ac4f9b5023f6dd",
+    "url": "/ErpReact/static/css/main.14a28586.chunk.css"
+  },
+  {
+    "revision": "577fc81b573439f2cddd",
+    "url": "/ErpReact/static/js/2.6abd0c2a.chunk.js"
+  },
+  {
+    "revision": "8b83e0ac4f9b5023f6dd",
+    "url": "/ErpReact/static/js/main.e8a353bf.chunk.js"
+  },
+  {
+    "revision": "a07dcba5b82a951c541f",
+    "url": "/ErpReact/static/js/runtime~main.808a4df5.js"
+  },
+  {
+    "revision": "a3505ec72fcc8ad507f5d79bf70a15af",
+    "url": "/ErpReact/static/media/neur.a3505ec7.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ErpReact/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ErpReact/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ErpReact/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ErpReact/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "70cede060d2df49e4a065acd4ca6b164",
+    "url": "/ErpReact/static/media/user-group.70cede06.png"
+  }
+]);
